f7f75843fb0dd32e4fe2d52e204bb93a9fa7645d
galer1 <robin.galeazzi@students.bfh.ch>
2023-12-20 00:06:14 +0100

    Added separate Skills Page and content to contact Page + section animation

--- scripts.js
+++ scripts.js
@@ -18,4 +18,46 @@ document.addEventListener('DOMContentLoaded', (event) => {
         // Aktualisiere die Position des skill-percent Elements
         skillPercent.style.left = `${newPosition - skillPercent.offsetWidth}px`;
     });
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            } else {
+                entry.target.classList.remove('in-view');
+            }
+        });
+    }, {
+        rootMargin: '0px',
+        threshold: 0.1
+    });
+
+    document.querySelectorAll('.section').forEach(section => {
+        observer.observe(section);
+    });
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Set up the Intersection Observer
+    const observer = new IntersectionObserver((entries) => {
+        // For each entry check if it is intersecting (visible)
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            } else {
+                entry.target.classList.remove('in-view');
+            }
+        });
+    }, {
+        // Adjust the root margin as needed to control when the animation starts
+        rootMargin: '0px',
+        threshold: 0.1 // Trigger when at least 10% of the section is visible
+    });
+
+    // Target the sections you want to observe
+    document.querySelectorAll('.section').forEach(section => {
+        observer.observe(section);
+    });
+});
+
+
+
