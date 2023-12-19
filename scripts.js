7aee15b8300c078b73d2c5f99971ee103e3db07c
galer1 <robin.galeazzi@students.bfh.ch>
2023-12-19 20:44:02 +0100

    About me page updated with skill bar

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
-window.addEventListener('scroll', () => {
+window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('.skill-percentage').forEach((elem) => {
-        if (elem.getBoundingClientRect().top < window.innerHeight) {
-            elem.classList.add('visible');
-        }
+        const percentage = elem.getAttribute('data-percentage');
+        elem.style.width = percentage;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Iteriere über alle Skill-Prozente
+    document.querySelectorAll('.skill-percent').forEach((skillPercent) => {
+        // Lese den prozentualen Wert des zugehörigen skill-percentage
+        const percentage = parseFloat(skillPercent.previousElementSibling.getAttribute('data-percentage'));
+        
+        // Berechne die neue Position basierend auf dem prozentualen Wert
+        const parentWidth = skillPercent.parentNode.offsetWidth;
+        const newPosition = (percentage / 100) * parentWidth;
+        
+        // Aktualisiere die Position des skill-percent Elements
+        skillPercent.style.left = `${newPosition - skillPercent.offsetWidth}px`;
     });
 });
